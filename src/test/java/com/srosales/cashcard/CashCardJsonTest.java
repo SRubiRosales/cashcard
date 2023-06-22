2fe7f0dc7186e439248ea86fd1ea51360df66ba8
SRubiRosales <sharon_rosales98@hotmail.com>
2023-06-22 12:47:44 -0600

    Testing deserialization

--- src/test/java/com/srosales/cashcard/CashCardJsonTest.java
+++ src/test/java/com/srosales/cashcard/CashCardJsonTest.java
@@ -13,7 +13,7 @@ public class CashCardJsonTest {
     @Autowired
     private JacksonTester<CashCard> json;
     @Test
-    public void myFirstTest() throws IOException {
+    public void cashCardSerializationTest() throws IOException {
         CashCard cashCard = new CashCard(99L, 123.45);
         assertThat(json.write(cashCard)).isStrictlyEqualToJson("expected.json");
         assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.id");
@@ -22,5 +22,19 @@ public class CashCardJsonTest {
         assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.amount");
         assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.amount")
                 .isEqualTo(123.45);
+    }
+
+    @Test
+    public void cashCardDeserializationTest() throws IOException {
+        String expected = """
+            {
+                "id":99,
+                "amount":123.45
+            }
+            """;
+        assertThat(json.parse(expected))
+                .isEqualTo(new CashCard(99L, 123.45));
+        assertThat(json.parseObject(expected).id()).isEqualTo(99L);
+        assertThat(json.parseObject(expected).amount()).isEqualTo(123.45);
     }
 }
