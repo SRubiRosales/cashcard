730519848dfcd8ec0ce28e7bc20fb6a45352718e
SRubiRosales <sharon_rosales98@hotmail.com>
2023-08-22 17:45:34 -0600

    Returning a list with GET

--- src/main/java/com/srosales/cashcard/CashCardController.java
+++ src/main/java/com/srosales/cashcard/CashCardController.java
@@ -1,10 +1,15 @@
 package com.srosales.cashcard;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -34,4 +39,10 @@ public class CashCardController {
                 .toUri();
         return ResponseEntity.created(locationOfNewCashCard).build();
     }
+
+    @GetMapping()
+    public ResponseEntity<Iterable<CashCard>> findAll() {
+        return ResponseEntity.ok(cashCardRepository.findAll());
+    }
+
 }
